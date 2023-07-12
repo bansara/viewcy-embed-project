4b21a98f10f5aec94f7dfbc80db7feb225b33dfe
Joshua Geisler <joshuageisler@joshuas-mbp.mynetworksettings.com>
2023-07-12 11:35:52 -0400

    added iframe resize script

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -68,6 +68,7 @@ const List = () => {
           </Link>
         </div>
       )}
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.2/iframeResizer.contentWindow.min.js"></script>
     </main>
   );
 };
